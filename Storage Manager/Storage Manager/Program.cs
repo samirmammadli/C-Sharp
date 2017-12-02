7b4ad3a880d00214bcc17ebd62c991ed028a704c
samir4ik86 <samir4ik86@gmail.com>
2017-12-02 21:38:52 +0400

    1

---
+++ Storage Manager/Storage Manager/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storage_Manager
+{
+    abstract class Storage
+    {
+        public string Name { get; protected set; }
+        protected int _writeSpeed;
+        protected int _readSpeed;
+
+        public Storage(string name, int writespeed, int readspeed)
+        {
+            _writeSpeed = writespeed;
+            _readSpeed = readspeed;
+        }
+    }
+
+    class Dvd : Storage
+    {
+    }
+
+internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
