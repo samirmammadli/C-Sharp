21d7d3b47d4b57717301816b5f25a1d127f377ac
Samir <Samir4ik86@gmail.com>
2017-12-19 19:09:28 +0400

    1

---
+++ Files/Files/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo original = new FileInfo(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+           var u = Directory.GetCurrentDirectory();
+            var path = 
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Desktop";
+            DirectoryInfo dir = new DirectoryInfo(u);
+            var dirs = dir.GetDirectories();
+            var files = dir.GetFiles();
+
+            Console.WriteLine(u);
+
+            foreach (var item in dirs)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            foreach (var item in files)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
