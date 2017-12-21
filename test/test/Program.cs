c4e17317733bbd5e1b38fb9fdd2a49ccbae02115
samir4ik86 <samir4ik86@gmail.com>
2017-12-21 21:50:00 +0400

    2

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace test
 {
+
     class Person
     {
         public string Name { get; set; }
@@ -31,40 +38,74 @@ namespace test
 
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-
-            List<Test> list = new List<Test>() {
-                new Test("Samir", 1986),
-                new Test("Ujal", 1995),
-                new Test("OOOOOr", 1986),
-                new Test("AAAlon", 1986),
-                new Test("Irvinn", 1995) };
-
-            //var hehe = from item3 in list[0].FullName
-            //           group
-
-
-            var poisk = (from item1 in list
-                                group item1 by item1.FullName).ToList<Test>();
-                        //where groups.Count() == 5
-                        //select groups;
-
-            foreach (var item in poisk)
+            var language = "en-ru";
+            var text = "";
+            var webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+            while (true)
             {
-                Console.WriteLine(item.FullName); 
+                text = Console.ReadLine();
+                text = HttpUtility.UrlEncode(text);
+                string url =
+                    $"https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20171221T171359Z.7c952e5cee5cce49.4522dd804535b842a0959725f4275cc4bf261f60&text={text}&lang={language}";
+                var data = webClient.DownloadString(url);
+                dynamic obj = JObject.Parse(data);
+                Console.WriteLine(obj.text[0]);
+                Console.Read();
+                Console.Clear();
             }
-
-            var group = from item2 in list
-                        where item2.FullName.GroupBy(x => x).OrderByDescending(z => z.Count()).First().Count() == 5
-                select item2;
-
-            //foreach (var item in group)
-            //{
-            //    Console.WriteLine(item.FullName);
-            //}
+            
         }
-
     }
 }
+
+
+//List<Test> list = new List<Test>() {
+//new Test("Samir", 1986),
+//new Test("Ujal", 1995),
+//new Test("OOOOOr", 1986),
+//new Test("AAAlon", 1986),
+//new Test("Irvinn", 1995) };
+
+////var hehe = from item3 in list[0].FullName
+////           group
+
+
+////string lesh = "Ahmedova irina agu";
+
+////var slovo = from item4 in lesh
+////    group item4 by x => x;
+
+////foreach (var r in slovo)
+////{
+////    Console.WriteLine(r.Key);
+////}
+
+////var poisk = from item1 in list
+////    group item1.FullName by 
+////    into g
+
+////    select g;
+////where groups.Count() == 5
+////select groups;
+
+////foreach (var item in poisk)
+////{
+////    Console.WriteLine(item.Key); 
+////}
+
+//var group = from item2 in list
+//where item2.FullName.GroupBy(x => x).OrderByDescending(z => z.Count()).First().Count() == 5
+//select item2;
+
+
+//Console.WriteLine(group.Count());
+//foreach (var item in group)
+//{
+//Console.WriteLine(item.FullName);
+//}
+
+//}
