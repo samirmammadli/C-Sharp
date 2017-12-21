edcb54bf881eec7c5836936ac7e4006ff5befb6e
Samir <Samir4ik86@gmail.com>
2017-12-21 17:32:48 +0400

    1

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -18,26 +18,52 @@ namespace test
         }
     }
 
+    class Test
+    {
+        public string FullName;
+        public int Year;
+        public Test(string name, int year)
+        {
+            FullName = name;
+            Year = year;
+        }
+    }
+
     class Program
     {
         
         static void Main(string[] args)
         {
 
-            DateTime time = new DateTime();//Time);
-            //time.AddSeconds();
-            time = time.AddSeconds(1513704134);
-            Console.WriteLine(time);
-            Person person = new Person { Name = "Tom", Age = 33 };
+            List<Test> list = new List<Test>() {
+                new Test("Samir", 1986),
+                new Test("Ujal", 1995),
+                new Test("OOOOOr", 1986),
+                new Test("AAAlon", 1986),
+                new Test("Irvinn", 1995) };
 
-            (string name, int age) = person;
-
-            Console.WriteLine(name);    // Tom
-            Console.WriteLine(age);     // 33
+            //var hehe = from item3 in list[0].FullName
+            //           group
 
 
-            var tuple = (name, age);
-            Console.WriteLine(tuple);
+            var poisk = (from item1 in list
+                                group item1 by item1.FullName).ToList<Test>();
+                        //where groups.Count() == 5
+                        //select groups;
+
+            foreach (var item in poisk)
+            {
+                Console.WriteLine(item.FullName); 
+            }
+
+            var group = from item2 in list
+                        where item2.FullName.GroupBy(x => x).OrderByDescending(z => z.Count()).First().Count() == 5
+                select item2;
+
+            //foreach (var item in group)
+            //{
+            //    Console.WriteLine(item.FullName);
+            //}
         }
 
     }
