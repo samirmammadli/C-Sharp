3024e3e4287b1f1f1914dc7f6b746e28c79d5e07
Samir <Samir4ik86@gmail.com>
2018-01-22 16:35:54 +0400

    1

---
+++ PulSifarishi/PulSifarishi/PulSifarishi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace PulSifarishi
+{
+    
+
+    class PulSifarishi
+    {
+        public string Header { get; set; }
+        public string Content { get; set; }
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Note { get; set; }
+    }
+}
