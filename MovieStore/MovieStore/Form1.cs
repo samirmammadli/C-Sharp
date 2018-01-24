88a72850644f0985cf73b7cc6656dc0a49a0a8b1
Samir <Samir4ik86@gmail.com>
2018-01-24 12:54:00 +0400

    1

--- MovieStore/MovieStore/Form1.cs
+++ MovieStore/MovieStore/Form1.cs
@@ -14,42 +14,76 @@ namespace MovieStore
 {
     public partial class Form1 : Form
     {
-        public BindingSource data;
+
+        private BindingSource data;
         private List<Movie> movies;
         private BindingSource bs;
+        private MovieStore store;
         public Form1()
         {
             InitializeComponent();
                 Random a = new Random();
+            store = new MovieStore();
             movies = new List<Movie>
             {
-                new Movie("Terminator 2", "Fantastic", "Movie", "240", "1991", false),
-                new Movie("Avatar", "Fantastic", "Movie", "240", "240", true)
+                new Movie("Terminator 2", "Fantastic", "Movie", 240, 1991, false),
+                new Movie("Avatar", "Fantastic", "Movie", 300, 2008, true)
             };
 
-            for (int i = 0; i < 321412; i++)
-            {
-                movies.Add(new Movie("Avatar", "Fantastic", "Movie", "240", a.Next(0, 2020).ToString(), true));
-            }
+            store.AddMovie(new Movie("Terminator 2", "Fantastic", "Movie", 240, 1991, false));
+            store.AddMovie(new Movie("Avatar2", "Fantastic", "Movie", 300, 2008, false));
+            store.AddMovie(new Movie("Lethal Weapon", "Action film", "Movie", 180, 1992, false));
+            store.AddMovie(new Movie("Fantastic four", "Fantastic", "Movie", 190, 2004, false));
+            store.AddMovie(new Movie("Millionare", "Drama", "Movie", 110, 1991, false));
+            store.AddMovie(new Movie("Game of Thrones", "Fantastic", "Serial", 65, 2017, false));
 
-            SortData(tbGenre.Text, tbTitle.Text, tbType.Text, tbYear.Text);
-            //data = new BindingSource {DataSource = movies};
-
-            // dataGridView2.DataSource = data;
+            DataView dw = new DataView(ConvertToDataTable(store.Movies));
+            bs = new BindingSource();
+            bs.DataSource = dw;
+            dataGridView2.DataSource = bs;
+            dataGridView2.Columns[0].Visible = false;
+            dataGridView2.Columns[1].Visible = false;
 
 
         }
 
+        public DataTable ConvertToDataTable<T>(IList<T> data)
+        {
+            PropertyDescriptorCollection properties =
+               TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable();
+            foreach (PropertyDescriptor prop in properties)
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            foreach (T item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+
         private void SortData(string genre, string title, string type, string year)
         {
-            var mov = (from item in movies.Take(20)
-                where item.Title.Contains(title, StringComparison.OrdinalIgnoreCase)
-                where item.Genre.Contains(genre, StringComparison.OrdinalIgnoreCase)
-                where item.Type.Contains(type, StringComparison.OrdinalIgnoreCase)
-                where item.Year.Contains(year, StringComparison.OrdinalIgnoreCase)
-                select item).ToList();
-            bs = new BindingSource { DataSource = mov };
-            dataGridView2.DataSource = bs;
+            //temp = (from item in movies
+            //        where item.Title.Contains(title, StringComparison.OrdinalIgnoreCase)
+            //        where item.Genre.Contains(genre, StringComparison.OrdinalIgnoreCase)
+            //        where item.Type.Contains(type, StringComparison.OrdinalIgnoreCase)
+            //        where (year == string.Empty || item.Year == year)
+            //        select item).Take(20).ToList();
+            //bs?.ResetBindings(false);
+
+            //if (int.Parse(year))
+
+
+
+            if (!int.TryParse(year, out int Year))
+                bs.Filter = $"Title LIKE '{title}%' AND Type LIKE '{type}%'";
+            else
+                bs.Filter = $"Title LIKE '{title}%' AND Type LIKE '{type}%' AND Year = {Year}";
+
+
         }
 
         private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
--- MovieStore/MovieStore/Movie.cs
+++ MovieStore/MovieStore/Movie.cs
@@ -14,22 +14,24 @@ namespace MovieStore
     {
         public static bool Contains(this string source, string toCheck, StringComparison comp)
         {
-            return source?.IndexOf(toCheck, comp) >= 0;
+            return source?.IndexOf(toCheck, 0, comp) == 0;
         }
     }
 
     class Movie
     {
         [Browsable(false)]
-        Image MovieImage { get; set; }
+        public Image MovieImage { get; private set; }
+        [Browsable(false)]
+        public int MovieID { get; set; }
         public string Title { get; set; }
         public string Genre { get; set; }
         public string Type { get; set; }
-        public string Runtime { get; set; }
-        public string Year { get; set; }
+        public int Runtime { get; set; }
+        public int Year { get; set; }
         public bool Viewed { get; set; }
 
-        public Movie(string title, string genre, string type, string runtime, string year, bool viewed, Image image = null)
+        public Movie(string title, string genre, string type, int runtime, int year, bool viewed, Image image = null)
         {
             Title = title;
             Genre = genre;
@@ -46,28 +48,49 @@ namespace MovieStore
         public delegate void MovieCollectionStateHandler();
         public event MovieCollectionStateHandler MovieCollectionChanged;
         private static int IdCounter = 0;
-        public Dictionary<int, Movie> Movies { get; private set; }
+        public List<Movie> Movies { get; private set; }
+
+        public MovieStore()
+        {
+            Movies = new List<Movie>();
+        }
 
         public void AddMovie(Movie movie)
         {
             if (movie == null) throw new ArgumentNullException();
-            Movies.Add(++IdCounter, movie);
+            Movies.Add(movie);
+            Movies.Last().MovieID = ++IdCounter;
             MovieCollectionChanged?.Invoke();
         }
 
         public void DeleteMovie(int id)
         {
-            if (!Movies.ContainsKey(id)) throw new ArgumentException("Wrong Movie ID!");
-            Movies.Remove(id);
-            MovieCollectionChanged?.Invoke();
+            try
+            {
+                Movies.RemoveAt(Movies.FindIndex(x => x.MovieID == id));
+                MovieCollectionChanged?.Invoke();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public void EditMovie(int id, Movie movie)
         {
-            if (!Movies.ContainsKey(id)) throw new ArgumentException("Wrong Movie ID!");
-            Movies[id] = movie;
-            MovieCollectionChanged?.Invoke();
-        }
+            if (movie == null) throw new ArgumentNullException();
+            try
+            {
+                int index = Movies.FindIndex(x => x.MovieID == id);
+                Movies.RemoveAt(index);
+                MovieCollectionChanged?.Invoke();
+            }
+            catch (Exception)
+            {
 
+                throw;
+            }
+        }
     }
 }
