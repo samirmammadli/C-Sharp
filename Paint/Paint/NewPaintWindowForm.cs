128997f6ba8a46bae4ed8b620be27acb2f41d261
Samir <Samir4ik86@gmail.com>
2018-01-29 10:44:11 +0400

    1

---
+++ Paint/Paint/NewPaintWindowForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class NewPaintWindowForm : Form
+    {
+        public NewPaintWindowForm()
+        {
+            InitializeComponent();
+        }
+
+        private void NewPaintWindowForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var result = MessageBox.Show("Do you want to save changes?", "Paint", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+            if (result == DialogResult.Cancel)
+                e.Cancel = true;
+        }
+    }
+}
