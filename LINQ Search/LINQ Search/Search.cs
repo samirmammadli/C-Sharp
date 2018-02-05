34b93c5123cbccbe31f3f2219171b3b653183d84
Samir <Samir4ik86@gmail.com>
2018-02-05 15:44:39 +0400

    1

---
+++ LINQ Search/LINQ Search/Search.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace LINQ_Search
+{
+    class Search
+    {
+        public void LoadUsers()
+        {
+            List<User> users = new List<User>();
+
+            XElement root = XElement.Load("../../XMLFile1.xml");
+
+            XmlSerializer formatter = new XmlSerializer(typeof(User));
+            foreach (var item in root.Elements())
+            {
+                using (MemoryStream stringInMemoryStream = new MemoryStream(ASCIIEncoding.Default.GetBytes(item.ToString())))
+                {
+                    try
+                    {
+                        users.Add(formatter.Deserialize(stringInMemoryStream) as User);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(item);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LINQ Search/LINQ Search/User.cs
@@ -0,0 +1,242 @@
+﻿
+namespace LINQ_Search
+{
+
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class User
+    {
+
+        private uint idField;
+
+        private string nameField;
+
+        private string lastNameField;
+
+        private string emailField;
+
+        private string genderField;
+
+        private string cityField;
+
+        private string raceField;
+
+        private string jobField;
+
+        private string countryField;
+
+        private decimal latField;
+
+        private decimal lonField;
+
+        private string departmentField;
+
+        private string companyField;
+
+        private string languageField;
+
+        private string birthDateField;
+
+        /// <remarks/>
+        public uint Id
+        {
+            get
+            {
+                return this.idField;
+            }
+            set
+            {
+                this.idField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string LastName
+        {
+            get
+            {
+                return this.lastNameField;
+            }
+            set
+            {
+                this.lastNameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Email
+        {
+            get
+            {
+                return this.emailField;
+            }
+            set
+            {
+                this.emailField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Gender
+        {
+            get
+            {
+                return this.genderField;
+            }
+            set
+            {
+                this.genderField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string City
+        {
+            get
+            {
+                return this.cityField;
+            }
+            set
+            {
+                this.cityField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Race
+        {
+            get
+            {
+                return this.raceField;
+            }
+            set
+            {
+                this.raceField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Job
+        {
+            get
+            {
+                return this.jobField;
+            }
+            set
+            {
+                this.jobField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Country
+        {
+            get
+            {
+                return this.countryField;
+            }
+            set
+            {
+                this.countryField = value;
+            }
+        }
+
+        /// <remarks/>
+        public decimal Lat
+        {
+            get
+            {
+                return this.latField;
+            }
+            set
+            {
+                this.latField = value;
+            }
+        }
+
+        /// <remarks/>
+        public decimal Lon
+        {
+            get
+            {
+                return this.lonField;
+            }
+            set
+            {
+                this.lonField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Department
+        {
+            get
+            {
+                return this.departmentField;
+            }
+            set
+            {
+                this.departmentField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Company
+        {
+            get
+            {
+                return this.companyField;
+            }
+            set
+            {
+                this.companyField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Language
+        {
+            get
+            {
+                return this.languageField;
+            }
+            set
+            {
+                this.languageField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string BirthDate
+        {
+            get
+            {
+                return this.birthDateField;
+            }
+            set
+            {
+                this.birthDateField = value;
+            }
+        }
+    }
+
+
+
+
+}
