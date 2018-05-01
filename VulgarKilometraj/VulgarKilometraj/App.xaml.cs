95b60532d9200d88485a070daeef16b50d01a232
samir4ik86 <samir4ik86@gmail.com>
2018-05-01 16:06:07 +0400

    1

--- VulgarKilometraj/VulgarKilometraj/App.xaml.cs
+++ VulgarKilometraj/VulgarKilometraj/App.xaml.cs
@@ -13,5 +13,17 @@ namespace VulgarKilometraj
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(MyHandler);
+        }
+
+        static void MyHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            MessageBox.Show("MyHandler caught : " + e.Message);
+            MessageBox.Show($"Runtime terminating: {args.IsTerminating.ToString()}");
+        }
     }
 }
--- VulgarKilometraj/VulgarKilometraj/MainWindow.xaml.cs
+++ VulgarKilometraj/VulgarKilometraj/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,22 @@ namespace VulgarKilometraj
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     /// 
-    public class ColumnState
+    [Serializable]
+    public class ObservableObject : INotifyPropertyChanged
     {
+        [NonSerialized]
+        private PropertyChangedEventHandler _PropertyChanged;
 
+        public virtual event PropertyChangedEventHandler PropertyChanged
+        {
+            add { _PropertyChanged += value; }
+            remove { _PropertyChanged -= value; }
+        }
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            _PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
     }
 
 
@@ -34,41 +50,105 @@ namespace VulgarKilometraj
             InitializeComponent();
             vg = new VulgarSheet();
             this.DataContext = this;
-            DataTable.ItemsSource = vg.Tables;
+            DataTable.SelectedCellsChanged += CellSelected;
+        }
+
+        private void CellSelected(object sender, SelectedCellsChangedEventArgs e)
+        {
+            if (DataTable.SelectedItem is InfoTable)
+            {
+                vg.SelectedItem = DataTable.SelectedItem as InfoTable;
+                vg.SelectedItem.PropertyChanged += OnKilometerChaged;
+            }
+        }
+
+        private void OnKilometerChaged(object sender, PropertyChangedEventArgs e)
+        {
+            try
+            {
+                if (e.PropertyName == "Kilometers")
+                {
+                    vg.SelectedItem.PropertyChanged -= OnKilometerChaged;
+                    InfoTable.totalSum = 200;
+                    foreach (var item in vg.Tables)
+                    {
+                        InfoTable.totalSum -= item.Sum;
+                        item.Remains = InfoTable.totalSum;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
         {
             lbDate.Content = vg.month;
+            InfoTable.totalSum = 200;
             var tempList = new List<InfoTable>();
             var tmp = new DateTime(vg.CurDate.Year, vg.CurDate.Month, 1);
             for (int i = vg.CurDate.Month; i == vg.CurDate.Month;)
             {
-                tempList.Add(new InfoTable(tmp, 23.5 , 24.7, 15));
+                tempList.Add(new InfoTable(tmp, 23));
                 tmp = tmp.AddDays(1);
                 i = tmp.Month;
             }
             vg.Tables = new ObservableCollection<InfoTable>(tempList);
             DataTable.ItemsSource = vg.Tables;
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (vg.SelectedItem != null)
+            MessageBox.Show(vg.SelectedItem.Kilometers.ToString());
+        }
     }
 
-    public class InfoTable
+    public class InfoTable : ObservableObject
     {
+        public static double totalSum = 200;
+        public double TotalSum
+        {
+            get { return totalSum; }
+            set { totalSum = value; OnPropertyChanged(); }
+        }
+
         public static double KmToLitreRate = 0.11;
-        public DateTime date { get; set; }
-        public double kilometers { get; set; }
-        public double sum { get; set; }
-        public double remain { get; set; }
+        private int kilometers;
+        public DateTime Date { get; set; }
+        private double remain;
+        public double Remains
+        {
+            get { return remain; }
+            set { remain = value; OnPropertyChanged("Remains"); }
+        }
+        private double sum;
+        public double Sum
+        {
+            get { return sum; }
+            set
+            {
+                sum = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Kilometers
+        {
+            get { return kilometers; }
+            set { kilometers = value; Sum = kilometers * KmToLitreRate;
+                OnPropertyChanged();
+            }
+        }
 
         public InfoTable() { }
 
-        public InfoTable(DateTime date, double kilometers, double sum, double remain)
+        public InfoTable(DateTime date, short kilometers)
         {
-            this.date = date;
-            this.kilometers = kilometers;
-            this.sum = sum;
-            this.remain = remain;
+            this.Date = date;
+            this.Kilometers = kilometers;
         }
     }
 
@@ -80,8 +160,10 @@ namespace VulgarKilometraj
         }
     }
 
-    public class VulgarSheet
+    public class VulgarSheet : ObservableObject
     {
+        private InfoTable selectedItem;
+        public InfoTable SelectedItem { get { return selectedItem; } set { selectedItem = value; OnPropertyChanged(); } }
         public string month { get; set; }
         private DateTime curDate;
         public DateTime CurDate
@@ -92,13 +174,14 @@ namespace VulgarKilometraj
                 curDate = value;
                 curDate = new DateTime(curDate.Year, CurDate.Month, 1);
                 month = CurDate.ToString("yyyy il MMMM ", new System.Globalization.CultureInfo("az-AZ")) + "ayi ucun";
+                OnPropertyChanged();
             }
         }
-        public ObservableCollection<InfoTable> Tables { get; set; }
+        private ObservableCollection<InfoTable> tables;
+        public ObservableCollection<InfoTable> Tables { get { return tables; } set { tables = value; OnPropertyChanged(); } }
         public VulgarSheet()
         {
             CurDate = DateTime.Now;
-            Tables = new ObservableCollection<InfoTable>();
         }
     }
 }
