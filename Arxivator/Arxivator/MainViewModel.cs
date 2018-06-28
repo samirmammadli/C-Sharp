93ad2308d3b05bea274e4300207dfa6f858b0a61
samir4ik86 <samir4ik86@gmail.com>
2018-06-28 11:17:29 +0400

    1

--- Arxivator/Arxivator/MainViewModel.cs
+++ Arxivator/Arxivator/MainViewModel.cs
@@ -14,12 +14,23 @@ namespace Arxivator
 {
     public class MainViewModel : ViewModelBase
     {
+        private ObservableCollection<int> selectedThreadsCount;
+
+        public ObservableCollection<int> SelectedThreadsCount
+        {
+            get { return selectedThreadsCount; }
+            set { Set(ref selectedThreadsCount, value); }
+        }
+
+
         public MainViewModel()
         {
             Progress = new ObservableCollection<int>();
+            SelectedThreadsCount = new ObservableCollection<int>();
             for (int i = 0; i < 4; i++)
             {
                 Progress.Add(0);
+                SelectedThreadsCount.Add(i + 1);
             }
         }
 
