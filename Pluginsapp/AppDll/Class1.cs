de4156a1cac21cecdd5db00b8bb148a4d9bfa84a
samir4ik86 <samir4ik86@gmail.com>
2018-07-11 17:10:40 +0400

    1

--- Pluginsapp/AppDll/Class1.cs
+++ Pluginsapp/AppDll/Class1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
-namespace WeatherPlugin
+namespace AppDll
 {
-    public class Weather
+    public interface IPlugin
     {
+        string Request(string input);
     }
 }
---
+++ Pluginsapp/WeatherPlugin/Weather.cs
@@ -0,0 +1,25 @@
+﻿using AppDll;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherPlugin
+{
+    public class Weather : IPlugin
+    {
+        public string Request(string input)
+        {
+            using (var client = new WebClient())
+            {
+                var query = $@"http://api.openweathermap.org/data/2.5/weather?q={input}&units=metric&appid=bf39fad37848cac5e2f9d0106327cc98";
+                var answer = client.DownloadString(query);
+                return query;
+                //dynamic output = JsonConvert.DeserializeObject(answer);
+            }
+        }
+    }
+}
