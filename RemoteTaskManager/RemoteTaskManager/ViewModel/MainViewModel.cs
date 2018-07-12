d253df1379eb358b94e9a4e5eb58971f7e351cbf
samir4ik86 <samir4ik86@gmail.com>
2018-07-12 16:36:50 +0400

    1

--- RemoteTaskManager/RemoteTaskManager/ViewModel/MainViewModel.cs
+++ RemoteTaskManager/RemoteTaskManager/ViewModel/MainViewModel.cs
@@ -2,7 +2,9 @@
 using GalaSoft.MvvmLight.CommandWpf;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,37 @@ namespace RemoteTaskManager.ViewModel
 {
     class MainViewModel : ViewModelBase
     {
+        static string ip = "127.0.0.1";
+        static int port = 8005;
+        private TcpClient client;
+
+        public MainViewModel()
+        {
+            client = new TcpClient();
+            client.Connect(ip, port);
+        }
+
+        private RelayCommand<string> sendMessage;
+        public RelayCommand<string> SendMessage
+        {
+            get
+            {
+                return sendMessage ?? (sendMessage = new RelayCommand<string>(param =>
+                SendMsg(param)));
+            }
+        }
+
+        public void SendMsg(string message)
+        {
+            using (var stream = client.GetStream())
+            {
+                using (var writer = new StreamWriter(stream, Encoding.Unicode))
+                {
+                    writer.WriteLine(message);
+                    writer.Flush();
+                }
+            }
+        }
 
     }
 }
--- RemoteTaskManager/TaskManagerServer/Program.cs
+++ RemoteTaskManager/TaskManagerServer/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TaskManagerServer
 {
+    public class Message
+    {
+        public string IP { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class TaskServer
+    {
+        string msg;
+        static int port = 8005; // порт для приема входящих запросов
+        static string ip = "127.0.0.1";
+        public void StartServer()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            TcpListener server = new TcpListener(IPAddress.Parse(ip), port);
+            server.Start();
+            
+            while (true)
+            {
+                var listener = server.AcceptTcpClient();
+                string ip = listener.Client.RemoteEndPoint.ToString();
+                using (var stream = listener.GetStream())
+                {
+                    using (var reader = new StreamReader(stream, Encoding.Unicode))
+                    {
+                        msg = reader.ReadLine();
+                    }
+                }
+                Console.WriteLine(msg + "    " + ip);
+            }
+            
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            var srv = new TaskServer();
+            srv.StartServer();
         }
     }
 }
