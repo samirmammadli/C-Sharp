5e4cc51b219836e091155434e50775a913ebe18a
samir4ik86 <samir4ik86@gmail.com>
2018-07-23 11:47:25 +0400

    1

--- RemoteTaskManager/TaskManagerServer/Program.cs
+++ RemoteTaskManager/TaskManagerServer/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Net;
-using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using TasksInfo;
 using TasksManagarCommands;
--- RemoteTaskManager/TasksManagarCommands/ClientCommands.cs
+++ RemoteTaskManager/TasksManagarCommands/ClientCommands.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Net.Sockets;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Threading.Tasks;
 using TasksInfo;
 
 namespace TasksManagarCommands
