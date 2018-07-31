71fa567fd864ed80d9692a6096bac7193bdc19bf
samir4ik86 <samir4ik86@gmail.com>
2018-07-31 15:47:17 +0400

    1

--- Hooking/Hooking/Program.cs
+++ Hooking/Hooking/Program.cs
@@ -1,8 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+
+static class MyClass
+{
+    [DllImport("user32.dll")]
+    static extern int GetKeyNameText(int lParam, [Out] StringBuilder lpString,
+  int nSize);
+
+    [DllImport("user32.dll")]
+    public static extern short GetAsyncKeyState(int vKey);
+
+}
 
 namespace Hooking
 {
@@ -10,6 +24,19 @@ namespace Hooking
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Thread.Sleep(10);
+                for (Int32 i = 0; i < 255; i++)
+                {
+                    int keyState = MyClass.GetAsyncKeyState(i);
+                    if (keyState == 1 || keyState == -32767)
+                    {
+                        Console.WriteLine((Keys)i);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
